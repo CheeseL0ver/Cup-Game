a271441f4970ea4183de74659cede2da782aea10
Shandon Anderson <andersonshandon@gmail.com>
2019-10-30 18:37:57 -0400

    Window now fullscreen by defaul

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((800,600),pygame.RESIZABLE)
+screen = pygame.display.set_mode((800,600),pygame.FULLSCREEN)
 background = pygame.Surface(screen.get_size())
 background.fill((255,255,0))
 background = background.convert()
@@ -14,8 +14,23 @@ FPS = 30
 # How many seconds the "game" is played.
 playtime = 0.0
 
-size = 100
-pygame.draw.rect(screen,(255,140,252),(0,0,100,100))
+BLOCK_SIZE = 50
+rects = []
+RED   = (255,   0,   0)
+BLACK   = (0,   0,   0)
+
+# for x in range(14):
+#     rects.append( pygame.Rect(x*(BLOCK_SIZE+5), BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE) )
+#     rects[x].x = x*(BLOCK_SIZE + 5)
+
+for x in range(16):
+    rects.append( pygame.Rect(x*(BLOCK_SIZE+5), BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE) )
+    rects[x].x = x*(BLOCK_SIZE)
+    rects[x].y = 300
+
+selected = None
+
+
 while mainloop:
     # Do not go faster than this framerate.
     milliseconds = clock.tick(FPS) 
@@ -30,23 +45,42 @@ while mainloop:
             if event.key == pygame.K_ESCAPE:
                 mainloop = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            x,y = pygame.mouse.get_pos()
-            print('x: ' + str(x)) 
-            print('y: ' + str(y)) 
-        elif event.type == pygame.VIDEORESIZE:
-            print("Resized...%s %s" % event.size)
-            screen = pygame.display.set_mode(event.size,pygame.RESIZABLE)
-            screen.blit(pygame.transform.scale(background,event.size), (0,0))
-            #pygame.display.flip()
-            pygame.display.update()
+                for i, r in enumerate(rects):
+                        if r.collidepoint(event.pos):
+                            selected = i
+                            selected_offset_x = r.x - event.pos[0]
+                            selected_offset_y = r.y - event.pos[1]
+            # x,y = pygame.mouse.get_pos()
+            # print('x: ' + str(x)) 
+            # print('y: ' + str(y)) 
+        elif event.type == pygame.MOUSEBUTTONUP:
+                selected = None
+
+        elif event.type == pygame.MOUSEMOTION:
+            if selected is not None: # selected can be `0` so `is not None` is required
+                # move object
+                rects[selected].x = event.pos[0] + selected_offset_x
+                rects[selected].y = event.pos[1] + selected_offset_y
 
                 
     # Print framerate and playtime in titlebar.
     text = "FPS: {0:.2f}   Playtime: {1:.2f}".format(clock.get_fps(), playtime)
     pygame.display.set_caption(text)
 
+    screen.fill((0,255,0))
+    index = 0
+    for r in rects:
+        if (index % 2 == 0):
+            pygame.draw.rect(screen, RED, r)
+        else:
+            pygame.draw.rect(screen, BLACK, r)
+
+        index += 1
+
     #Update Pygame display.
-    pygame.display.flip()
+    pygame.display.update()
+
+    
 
 # Finish Pygame.  
 pygame.quit()
