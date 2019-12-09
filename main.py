74a0c9d73fb49dce7f8611c4a97bce86e9c3724f
Shandon Anderson <andersonshandon@gmail.com>
2019-12-08 23:26:24 -0500

    Attempting to implement move logic

--- main.py
+++ main.py
@@ -12,6 +12,22 @@ class Stack(pygame.Rect):
     def __str__(self):
         return ('Stack Count: %s\nLast Position: %s\nLast Position Index: %s\nCurrent Position: %s\nCurrent Position Index: %s' % (self.stackCount, self.lastPosition, self.lastPositionIndex, self.currentPosition, self.currentPositionIndex))
 
+def centerStacks():
+    #Center stacks
+    for r in rects:
+        for c in cups:
+            if r.colliderect(c):
+                c.center = r.center
+
+def isValidMove(stack):
+    distance = stack.currentPositionIndex - stack.lastPositionIndex
+    print(stack)
+    print("Dist: %s" % distance)
+    if abs(distance) == stack.stackCount:
+        return True
+
+    return False
+
 YELLOW = (255,255,0)
 GREEN = (0, 255, 0)
 pygame.init()
@@ -46,12 +62,18 @@ for x in range(23):
 
 #Valid positions
 validPositions = [r.center for r in rects]
-print(validPositions)
 
 for x in range(23):
     cups.append( Stack(x*(BLOCK_SIZE+5), BLOCK_SIZE, BLOCK_SIZE / 2, BLOCK_SIZE / 2) )
     cups[x].x = x*(BLOCK_SIZE)
     cups[x].y = 300
+
+centerStacks()
+for stack in cups:
+    stack.lastPosition = stack.center
+    stack.lastPositionIndex = validPositions.index(stack.lastPosition)
+    stack.currentPosition = stack.lastPosition
+    stack.currentPositionIndex = stack.lastPositionIndex
 
 selected = None
 
@@ -72,20 +94,14 @@ while mainloop:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             x,y = pygame.mouse.get_pos()
             if event.button == 1:
-                for i, r in enumerate(cups):
-                    if r.collidepoint(event.pos):
+                for i, stack in enumerate(cups):
+                    if stack.collidepoint(event.pos):
                         selected = i
-                        selected_offset_x = r.x - event.pos[0]
-                        selected_offset_y = r.y - event.pos[1]
-                        print("Stack count: %s" % r.stackCount)
-                        r.lastPosition = r.center
-                        r.lastPositionIndex = validPositions.index(r.lastPosition)
-                        for dex, c in enumerate(cups):
-                            if dex is not i and c.colliderect(r): #One stack collects another
-                                c.stackCount += r.stackCount
-                                c.currentPosition = c.center
-                                c.currentPositionIndex = validPositions.index(c.currentPosition)
-                                cups.remove(r)
+                        selected_offset_x = stack.x - event.pos[0]
+                        selected_offset_y = stack.y - event.pos[1]
+                        print("Stack count: %s" % stack.stackCount)
+                        stack.lastPosition = stack.center
+                        stack.lastPositionIndex = validPositions.index(stack.lastPosition)
 
         elif event.type == pygame.MOUSEBUTTONUP:
                 selected = None
@@ -93,8 +109,31 @@ while mainloop:
                     if event.button == 1:
                         for dex, c in enumerate(cups):
                             if dex is not i and c.colliderect(r): #One stack collects another
+                                centerStacks()
+                                # c.currentPosition = c.center
+                                c.currentPosition = r.currentPosition
+                                c.currentPositionIndex = r.currentPositionIndex
+                                # c.currentPositionIndex = validPositions.index(c.currentPosition)
+                                #print(c.stackCount)
+                                #print(r.stackCount)
                                 c.stackCount += r.stackCount
                                 cups.remove(r)
+                                """
+                                if isValidMove(c):
+                                    print("valid")
+                                    print(c.stackCount)
+                                    c.stackCount += r.stackCount
+                                    print(c.stackCount)
+                                    cups.remove(r)
+                                else:
+                                    print("NOT valid")
+                                    # c.stackCount += r.stackCount
+                                    # print(c.stackCount)
+                                    # cups.remove(r)
+                                    r.currentPosition = r.lastPosition
+                                """
+
+                # Ensure position is on the "board"
                 for c in cups:
                     if not c.center in validPositions:
                         c.center = c.lastPosition
@@ -112,6 +151,8 @@ while mainloop:
 
     screen.fill(WHITE)
     index = 0
+
+    #Redraw game board
     for r in rects:
         if (index % 2 == 0):
             pygame.draw.rect(screen, YELLOW, r)
@@ -120,17 +161,12 @@ while mainloop:
 
         index += 1
 
+    #Redraw stacks
     for r in cups:
             pygame.draw.circle(screen, BLUE, r.center, 20)
             screen.blit(font.render(str(r.stackCount), True, (255,255,255)), (r.left, r.top))
 
-    #Center squares
-    for r in rects:
-        for c in cups:
-            if r.colliderect(c):
-                c.center = r.center
-
-
+    centerStacks()
 
     #Update Pygame display.
     pygame.display.update()
